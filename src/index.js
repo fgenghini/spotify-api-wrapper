56e00a1546fee4f3d639df031c9a32813db6f208
Fernando Genghini <fgenghini@ciandt.com>
2018-01-07 17:59:49 -0200

    - Add build dependencies - Create build commands - Create entrypoint script (index.js)

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {
+  album,
+  albums,
+  albumTracks,
+} from './album';
+
+import {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from './search';
+
+module.exports = {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+  album,
+  albums,
+  albumTracks,
+};
