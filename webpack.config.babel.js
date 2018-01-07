13605c32a6019893838acb5bc1e35f318df32fbf
Fernando Genghini <fgenghini@ciandt.com>
2018-01-07 18:22:54 -0200

    Add webpack and create build commands for UMD format

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+import { join } from 'path';
+
+const include = join(__dirname, 'src');
+const outputPath = join(__dirname, 'dist');
+
+export default {
+  entry: './src/index',
+  output: {
+    path: outputPath,
+    libraryTarget: 'umd',
+    library: 'SpotifyAPIWrapper',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', include },
+    ],
+  },
+};
